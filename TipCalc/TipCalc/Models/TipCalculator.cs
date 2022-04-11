65a055b8da37ec8fe08a396fc156854a7bc28dea
Borsodi Ádám <91759206+adamb00@users.noreply.github.com>
2022-04-11 21:25:08 +0200

    Add files via upload

---
+++ TipCalc/TipCalc/Models/TipCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalc.Models
+{
+    public class TipCalculator
+    {
+
+        private double price;
+
+
+        public TipCalculator(double price)
+        {
+            this.price = price;
+
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        public double Tip
+        {
+            get { return price * 0.10; }
+        }
+
+        public override string ToString()
+        {
+            string result = "A számla végösszege: " + price + "\n";
+            result += "Elvárt borravaló: " + Tip + "\n";
+            result += "A számla 10% borravalóval " + (price + Tip);
+            return result.ToString();
+        }
+
+
+    }
+}
