7979b712f9de8a73e918590c78f5ed7afbdfe7f1
Borsodi Ádám <91759206+adamb00@users.noreply.github.com>
2022-04-11 21:26:42 +0200

    Add files via upload

---
+++ TipCalc/TipCalc/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.IO;
+using TipCalc.Models;
+
+namespace TipCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A számla végösszege?  ");
+            TipCalculator t = new TipCalculator(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(t);
+            Console.ReadKey();
+        }
+    }
+}
